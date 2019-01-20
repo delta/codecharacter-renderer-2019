89a76ad4f93c049b3d5fc3079865e5aff079e94b
neel <theofficialneel@gmail.com>
2019-01-20 19:29:04 +0530

    commented out old code for ownership

--- src/javascripts/state_objects/terrain.js
+++ src/javascripts/state_objects/terrain.js
@@ -7,69 +7,79 @@ export default class TerrainElement extends StateObject {
         super(x, y, TerrainElement.sideLength, TerrainElement.sideLength, TerrainElement.textures.landTexture);
 
         this.playerID = 0;
-        this.nearbyTowers = { 1: {}, 2: {} };
+        // this.nearbyTowers = { 1: {}, 2: {} };
 
         this.overlay = new GraphicsPrimitive(x, y, TerrainElement.sideLength, TerrainElement.sideLength, terrianType);
     }
 
-    addOwnership(playerID, towerID) {
-        this.addTower(playerID, towerID);
-
-        if (this.playerID == playerID || this.playerID == 3)
-            return;
-
-        if (this.playerID === 0)
-            this.playerID = playerID;
-        else this.playerID = 3;
-
-        TerrainElement.addOwnership(playerID);
-        this.overlay.fill(this.playerID);
-    }
-
-    removeOwnership(playerID, towerID) {
-        if (this.playerID == 0 || (this.playerID != 3 && this.playerID != playerID))
-            return;
-
-        this.removeTower(playerID, towerID);
-
-        if ( Object.keys(this.getNearbyTowers(playerID)).length === 0 ) {
-            TerrainElement.removeOwnership(playerID);
-            this.playerID -= playerID;
-            this.overlay.fill(this.playerID);
+    static getTerrainTexture(terrianType) {
+        if (terrianType === 1) {
+            return TerrainElement.textures.waterTexture;
+        } else if (terrianType === 2) {
+            return TerrainElement.textures.goldTexture;
+        } else {
+            return TerrainElement.textures.landTexture;
         }
     }
 
-    addTower(playerID, towerID) {
-        this.nearbyTowers[playerID][towerID] = null;
-    }
+    // addOwnership(playerID, towerID) {
+    //     this.addTower(playerID, towerID);
 
-    removeTower(playerID, towerID) {
-        delete this.nearbyTowers[playerID][towerID];
-    }
+    //     if (this.playerID == playerID || this.playerID == 3)
+    //         return;
 
-    getNearbyTowers(playerID) {
-        return this.nearbyTowers[playerID];
-    }
+    //     if (this.playerID === 0)
+    //         this.playerID = playerID;
+    //     else this.playerID = 3;
+
+    //     TerrainElement.addOwnership(playerID);
+    //     this.overlay.fill(this.playerID);
+    // }
+
+    // removeOwnership(playerID, towerID) {
+    //     if (this.playerID == 0 || (this.playerID != 3 && this.playerID != playerID))
+    //         return;
+
+    //     this.removeTower(playerID, towerID);
+
+    //     if ( Object.keys(this.getNearbyTowers(playerID)).length === 0 ) {
+    //         TerrainElement.removeOwnership(playerID);
+    //         this.playerID -= playerID;
+    //         this.overlay.fill(this.playerID);
+    //     }
+    // }
+
+    // addTower(playerID, towerID) {
+    //     this.nearbyTowers[playerID][towerID] = null;
+    // }
+
+    // removeTower(playerID, towerID) {
+    //     delete this.nearbyTowers[playerID][towerID];
+    // }
+
+    // getNearbyTowers(playerID) {
+    //     return this.nearbyTowers[playerID];
+    // }
 
 
-    static createOwnershipObject() {
-        this.ownership = {
-            1: 0,
-            2: 0
-        };
-    }
+    // static createOwnershipObject() {
+    //     this.ownership = {
+    //         1: 0,
+    //         2: 0
+    //     };
+    // }
 
-    static addOwnership(playerID) {
-        this.ownership[playerID] += 1;
-    }
+    // static addOwnership(playerID) {
+    //     this.ownership[playerID] += 1;
+    // }
 
-    static removeOwnership(playerID) {
-        this.ownership[playerID] -= 1;
-    }
+    // static removeOwnership(playerID) {
+    //     this.ownership[playerID] -= 1;
+    // }
 
-    static getOwnership() {
-        return this.ownership;
-    }
+    // static getOwnership() {
+    //     return this.ownership;
+    // }
 
     static setSideLength(len) {
         this.sideLength = len;
@@ -77,7 +87,9 @@ export default class TerrainElement extends StateObject {
 
     static setTextures() {
         this.textures = {
-            landTexture: PIXI.loader.resources.land.texture
+            landTexture: PIXI.loader.resources.land.texture,
+            waterTexture: PIXI.loader.resources.land.texture,
+            goldTexture: PIXI.loader.resources.land.texture
         };
     }
 
